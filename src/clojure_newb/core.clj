f88db26c361b8fcca218c3c25d762f888213a379
Kyle Kincer <kylekincer@gmail.com>
2025-06-23 16:44:52 -0400

    problem2

--- src/clojure_newb/core.clj
+++ src/clojure_newb/core.clj
@@ -13,16 +13,20 @@
                       #(or (zero? (mod % 3)) (zero? (mod % 5)))
                       (range 1 1000)))))
 
-(defn problem2
-  "project euler problem 2: By considering the terms in the Fibonacci sequence whose values do not exceed four million, find the sum of the even-valued terms."
-  [])
-
 (defn fib
   "fibonacci helper for problem2."
-  ([] (fib 0 1))
+  ([] (fib 0N 1N))
   ([a b]
    (lazy-seq
     (cons a (fib b (+ a b))))))
+
+(defn problem2
+  "project euler problem 2: By considering the terms in the Fibonacci sequence whose values do not exceed four million, find the sum of the even-valued terms."
+  []
+  (->> (fib)
+       (take-while #(<= % 4000000))
+       (filter even?)
+       (reduce +)))
 
 (defn -main
   [& args]
